715b8cee03c135a14e1b042312f8658e7f6978d3
Suui <suui.gd@gmail.com>
2019-01-15 01:30:53 +0100

    Mobile menu ready for action

--- js/events.js
+++ js/events.js
@@ -6,17 +6,32 @@ document.addEventListener('scroll', function(e) {
   } else {
     document.querySelector('header').style.background = 'rgba(0, 25, 50, 0)';
   }
+});
+
+document.getElementById('switch-icons').addEventListener('click', function(e) {
+  document.querySelector('.mobile-menu-left').classList.remove('toggled');
+  document.querySelector('.mobile-menu-right').classList.remove('toggled');
+  
+  var menu = document.querySelector('.mobile-menu');
+  if (menu.classList.contains('left-handed')) {
+    menu.classList.remove('left-handed');
+    return;
+  }
+  menu.classList.add('left-handed');
 })
 
-(function(header) {
-  'use strict';
-  document.getElementById('toggle').addEventListener('click', (e) => {
-    e.preventDefault();
-    header.classList.toggle('expanded');
-  });
-  header.querySelectorAll('li').forEach(e => {
-    e.addEventListener('click', () => {
-      header.classList.remove('expanded');
-    });
-  });
-}(document.querySelector('header')));
+document.getElementById('toggle-menu').addEventListener('click', function(e) {
+  var targetMenuClass = document.querySelector('.mobile-menu').classList.contains('left-handed') ? '.mobile-menu-left' : '.mobile-menu-right'
+  var rightMenu = document.querySelector(targetMenuClass);
+  if (rightMenu.classList.contains('toggled')) {
+    rightMenu.classList.remove('toggled');
+    return;
+  }
+  rightMenu.classList.add('toggled');
+});
+
+document.querySelectorAll('.mobile-menu-right a').forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    document.querySelector('.mobile-menu-right').classList.remove('toggled');
+  })
+});
